9a05be2cbccdd3e6265cac7cf93155c7c897b61c
Nimal Das <nimaldasps@gmail.com>
2023-05-19 11:59:58 +0530

    Updated test file

--- test_app.py
+++ test_app.py
@@ -1,4 +1,11 @@
 from app import app
 def test_HTTP():
-    response = app.test_client.get('/')
+    response = app.test_client().get('/')
     assert response.status_code == 200
+
+def test_webtxt():
+    response = app.test_client().get('/home')
+    assert b"Total" in response.data
+    assert b"Statistics" in response.data
+    assert b"Pending" in response.data
+    assert b"Update" in response.data
